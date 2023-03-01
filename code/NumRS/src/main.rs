d9935281ad466a94eb1d8e1f949fed28d0060fe9
Carlos Iborra Llopis <charlie.iborra@gmail.com>
2023-03-01 11:22:27 +0100

    🚧 In dev commit

--- code/NumRS/src/main.rs
+++ code/NumRS/src/main.rs
@@ -2,6 +2,11 @@
 
 
 #[no_mangle]  // Not to mangle the name of the function, not interfere with the name of the function in the Python code
-pub extern "C" fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub extern "C" fn sum(arr: *const i32) -> i32 {
+    /// Sum of numbers in an array
+    /// :param a: pointer to the array
+    /// :param n: length of the array
+    /// :return: sum of the array
+    let sum = arr.iter().fold(0, |acc, x| acc + x);
+    sum // return the sum
 }
--- tests/numpy.py
+++ tests/numpy.py
@@ -1,2 +1,40 @@
-import numpy
+''' Test numpy with ctypes.  Test to compare performance of NumPy vs NumRs '''
 
+from ctypes import cdll
+from time import time
+import random as rnd
+import numpy as np
+
+# Load NumRs library as rs
+rs = cdll.LoadLibrary("NumRs.dll")
+
+
+# Test 1: sum of 1 million random numbers
+def test1():
+    ''' Test sum of 1 million random numbers '''
+    start = time()
+    print("Test 1: sum of 1 million random numbers")
+    # Generate random numbers from 0 to 99 inclusive (100000 numbers)
+    nums = [rnd.randint(0, 99) for i in range(1000000)]
+    # Sum with NumPy
+    np_sum = np.sum(nums)
+    print("NumPy sum: ", np_sum)
+    print("NumPy time: ", time() - start)
+
+
+# Test 2: sum of 1 million random numbers with NumRs
+def test2():
+    ''' Test sum of 1 million random numbers '''
+    start = time()
+    print("Test 1: sum of 1 million random numbers")
+    # Generate random numbers
+    nums = [rnd.random() for i in range(1000000)]
+    # Sum with NumRs
+    rs_sum = rs.sum(nums, len(nums))
+    print("NumRs sum: ", rs_sum)
+    print("NumRs time: ", time() - start)
+
+
+# Execute tests
+test1()
+test2()
